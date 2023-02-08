be0badcbc1c11f149087ad037a827ce4a2ae6a39
Marek <m.arendt04@gmail.com>
2023-02-08 07:57:19 +0100

    More setup

--- src/config.py
+++ src/config.py
@@ -1,12 +1,13 @@
 from pydantic import BaseSettings
 
+
 class AppSettings(BaseSettings):
-    MONGO_USERNAME : str
-    MONGO_PASSWORD : str
-    MONGO_DATABASE : str
+    MONGO_USERNAME: str
+    MONGO_PASSWORD: str
+    MONGO_DATABASE: str
 
     class Config:
-        env_file = './.env'
+        env_file = "./.env"
 
 
 app_settings = AppSettings()
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from src.config import app_settings
 
+
 app = FastAPI()
 
+
 @app.get("/healthcheck")
-def read_root():
-    return {"asd": app_settings.MONGO_DATABASE}
+def healthcheck() -> dict:
+    return {}
