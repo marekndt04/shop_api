75d8287e5f19d6253b452c8e97c2848234dd988d
Marek <m.arendt04@gmail.com>
2023-02-26 10:37:58 +0100

    Created validator for MONGO_PORT attribute.

--- src/config.py
+++ src/config.py
@@ -1,12 +1,16 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, validator
 
 
 class AppSettings(BaseSettings):
     MONGO_USERNAME: str
     MONGO_PASSWORD: str
     MONGO_DATABASE: str
-    MONGO_PORT: str
+    MONGO_PORT: int
     MONGO_HOST: str = "localhost"
+
+    @validator("MONGO_PORT", pre=True)
+    def cast_port(cls, v):
+        return int(v)
 
     class Config:
         env_file = ".env"
--- src/database.py
+++ src/database.py
@@ -4,7 +4,7 @@ from .config import app_settings
 
 mongo_db_url = (
     f"mongodb://{app_settings.MONGO_USERNAME}:{app_settings.MONGO_PASSWORD}"
-    f"@{app_settings.MONGO_HOST}:{int(app_settings.MONGO_PORT)}"
+    f"@{app_settings.MONGO_HOST}:{app_settings.MONGO_PORT}"
 )
 mongo_client = motor_asyncio.AsyncIOMotorClient(mongo_db_url)
 db = mongo_client[app_settings.MONGO_DATABASE]
