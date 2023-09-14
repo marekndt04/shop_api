309350dddf696993808cf4b8f421a40fb3aae55a
Marek <m.arendt04@gmail.com>
2023-09-14 22:53:10 +0200

    updated charts, modified how settings use environment variables

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+import os
+
 from pydantic import validator
 from pydantic_settings import BaseSettings
 
@@ -13,8 +15,18 @@ class AppSettings(BaseSettings):
     def cast_port(cls, v):
         return int(v)
 
-    class Config:
-        env_file = ".env"
+
+mongo_username = os.getenv("MONGO_USERNAME")
+mongo_password = os.getenv("MONGO_PASSWORD")
+mongo_database = os.getenv("MONGO_DATABASE")
+mongo_port = os.getenv("MONGO_PORT")
+mongo_host = os.getenv("MONGO_HOST", "localhost")
 
 
-app_settings = AppSettings()
+app_settings = AppSettings(
+    MONGO_USERNAME=mongo_username,
+    MONGO_PASSWORD=mongo_password,
+    MONGO_DATABASE=mongo_database,
+    MONGO_PORT=mongo_port,
+    MONGO_HOST=mongo_host,
+)
