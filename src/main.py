dfa495f7689abd98f63acfc86b6c45b99ae9f9b4
Marek <m.arendt04@gmail.com>
2024-01-27 11:21:47 +0100

    Removed collection setup for test

--- src/main.py
+++ src/main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
 
-from src.database import setup_collections
 from src.products.views import router as products_router
 
 app = FastAPI()
-app.add_event_handler("startup", setup_collections)
 
 app.include_router(products_router)
 
